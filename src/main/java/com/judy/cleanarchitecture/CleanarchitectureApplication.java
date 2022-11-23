8c6828834ba4bc95b959b4e2184e5384833adada
Ara <60602858+arajo-hub@users.noreply.github.com>
2022-11-23 16:48:48 +0900

    프로젝트 생성

---
+++ src/main/java/com/judy/cleanarchitecture/CleanarchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.judy.cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchitectureApplication.class, args);
+	}
+
+}
